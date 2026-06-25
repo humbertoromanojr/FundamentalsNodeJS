5eaca53de0537c8377693ab453d3179593b5c46d
humbertoromanojr <astronomi@gmail.com>
2026-06-25 14:40:55 -0300

    Stream Readable

---
+++ streams/fundamentals.js
@@ -0,0 +1,19 @@
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = i;
+
+  _read() {
+    const i = this.index++;
+
+    if (i > 100) {
+      this.push(null);
+    } else {
+      const buf = Buffer.from(String(i));
+
+      this.push(buf);
+    }
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
