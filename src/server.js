0077ca5fa64953f323f54fef5834f377442e9dd8
humbertoromanojr <astronomi@gmail.com>
2026-06-29 12:18:15 -0300

    Learning Behind the Scenes: How an API Works

--- src/server.js
+++ src/server.js
@@ -1,1 +1,56 @@
-import http from "http";
+import http from "node:http";
+
+// Get => Buscar um recurso do backend
+// POST => Criar um recurso do backend
+// PUT => Atualizar um recurso no backend
+// PATCH => Atualizar uma informação específica de um recurso no backend
+// DELETE => Deletar um recurso no backend
+
+// Get /users => Buscando usuários do backend
+// POST /users => Criar um usuário no backend
+
+// Stateful ------- Stateless
+
+// JSON - Javascript Object Notation
+
+const users = [];
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    });
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end();
+});
+
+server.listen(3001, () => {
+  console.log("Server runner now in port 3001");
+});
